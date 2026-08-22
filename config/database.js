f018bbeb42e6d044b19f211d26f29b1e4fc29c81
jadenadm2-afk <jaden.adm2@gmail.com>
2026-08-22 13:12:05 +0300

    Add config/database.js

---
+++ config/database.js
@@ -0,0 +1,30 @@
+﻿require('dotenv').config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USER || 'postgres',
+    password: process.env.DB_PASS || '',
+    database: process.env.DB_NAME || 'survey_db',
+    host:     process.env.DB_HOST || 'localhost',
+    port:     parseInt(process.env.DB_PORT) || 5432,
+    dialect:  'postgres',
+    dialectOptions: {
+      ssl: process.env.DB_SSL === 'true' ? {
+        require: true,
+        rejectUnauthorized: false,
+      } : false,
+    },
+    logging: false,
+  },
+  production: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+    logging: false,
+  },
+};
