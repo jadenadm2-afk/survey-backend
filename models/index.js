784e5d4198cb36f23720ef987d42b3ffa26beb57
jadenadm2-afk <jaden.adm2@gmail.com>
2026-08-22 13:12:09 +0300

    Add models/index.js

---
+++ models/index.js
@@ -0,0 +1,25 @@
+﻿'use strict';
+const { Sequelize } = require('sequelize');
+const config        = require('../config/database');
+
+const env        = process.env.NODE_ENV || 'development';
+const dbConfig   = config[env];
+
+let sequelize;
+if (dbConfig.use_env_variable) {
+  sequelize = new Sequelize(process.env[dbConfig.use_env_variable], dbConfig);
+} else {
+  sequelize = new Sequelize(
+    dbConfig.database,
+    dbConfig.username,
+    dbConfig.password,
+    dbConfig
+  );
+}
+
+const db = {};
+db.Sequelize   = Sequelize;
+db.sequelize   = sequelize;
+db.SurveyResponse = require('./SurveyResponse')(sequelize);
+
+module.exports = db;
